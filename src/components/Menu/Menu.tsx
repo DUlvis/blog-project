7d01480fc2ec68654f91476d3fe129c5249c3e5a
Denis <ITUlvis@gmail.com>
2022-12-27 23:06:17 +0200

    add Menu

---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,17 @@
+import Button from '@mui/material/Button'
+
+type Props = {}
+
+const Menu = (props: Props) => {
+    return (
+        <>
+            <Button color="inherit">Home</Button>
+            <Button color="inherit">Blog</Button>
+            <Button color="inherit">Services</Button>
+            <Button color="inherit">Pricing</Button>
+            <Button color="inherit">About US</Button>
+        </>
+    )
+}
+
+export default Menu
