fcef82fab71b2acf1fac5057aba8624112774368
Denis <ITUlvis@gmail.com>
2023-02-21 21:34:31 +0200

    add pages

--- src/container/Main/Main.tsx
+++ src/container/Main/Main.tsx
@@ -3,6 +3,11 @@ import Home from 'pages/Home/Home'
 import Favorites from 'pages/Favorites/Favorites'
 import { Route, Routes } from 'react-router-dom'
 import './Main.scss'
+import AboutUs from 'pages/AboutUs/AboutUs'
+import Typography from 'pages/Typography/Typography'
+import Travel from 'pages/Travel/Travel'
+import Business from 'pages/Business/Business'
+import Contact from 'pages/Contact/Contact'
 
 type Props = {}
 
@@ -13,7 +18,12 @@ const Main = (props: Props) => {
                 <div className="main-inner">
                     <Routes>
                         <Route path="/" element={<Home />} />
+                        <Route path="about" element={<AboutUs />} />
+                        <Route path="typography" element={<Typography />} />
+                        <Route path="travel" element={<Travel />} />
+                        <Route path="business" element={<Business />} />
                         <Route path="favorites" element={<Favorites />} />
+                        <Route path="contact" element={<Contact />} />
                     </Routes>
                 </div>
             </Container>
---
+++ src/pages/Typography/Typography.tsx
@@ -0,0 +1,7 @@
+type Props = {}
+
+const Typography = (props: Props) => {
+    return <div>Typography</div>
+}
+
+export default Typography
